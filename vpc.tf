7a78e284b7dfc38d331deebc2e4e2e15f7f024af
yerrabandireddy <62086271+yerrabandireddy@users.noreply.github.com>
2020-06-23 06:34:53 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -27,6 +27,21 @@ resource "aws_subnet" "subnet_public" {
     Environment = var.environment_tag
   }
 }
+resource "aws_route_table" "rtb_public" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "terraform-rtb"
+    Environment = var.environment_tag
+  }
+}
+resource "aws_route_table_association" "rta_subnet_public" {
+  subnet_id      = aws_subnet.subnet_public.id
+  route_table_id = aws_route_table.rtb_public.id
+}
 
 
 
