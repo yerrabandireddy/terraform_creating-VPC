bd3514e0b393306d3ff6cde37845460679f8f1b7
yerrabandireddy <62086271+yerrabandireddy@users.noreply.github.com>
2020-06-23 06:26:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,14 @@ variable "environment_tag" {
   description = "Environment tag"
   default = "Dev"
   }
+
+
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default = "10.1.0.0/24"
+}
+
+variable "availability_zone" {
+  description = "availability zone to create subnet"
+  default = "ap-south-1a"
+}
