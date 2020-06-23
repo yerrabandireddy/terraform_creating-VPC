d8a78d00aeff0194a68aef3695f55672bb852f76
yerrabandireddy <62086271+yerrabandireddy@users.noreply.github.com>
2020-06-23 06:14:58 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,12 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr_vpc
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "terraform-vpc"
+    Environment = var.environment_tag
+  }
+}
+
+
 
